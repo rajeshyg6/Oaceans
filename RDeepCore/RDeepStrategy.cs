201fc61fcfa7a6f5b3ed50fad076bbe9f970ab66
rajeshyg6 <30372368+rajeshyg6@users.noreply.github.com>
2018-01-09 06:36:11 -0500

    SetProbabilityUpgradeFactorsOnHit
    SetProbabilityUpgradeFactorsOnHit

--- RDeepCore/RDeepStrategy.cs
+++ RDeepCore/RDeepStrategy.cs
@@ -27,14 +27,14 @@ namespace RDeepCore
     class BetByTenFifteenStrategy : IRDeepStrategy
     {
         Dictionary<int, float> probabilities;
-        Dictionary<PositonTypeCategory, List<int>> probabilityUpgradeFactors;
+        Dictionary<PositonTypeCategory, List<int>> probabilityUpgradeFactorsOnHit;
 
         IEnumerable<RDeepPosition> wheelNumbers;
 
         public BetByTenFifteenStrategy()
         {
-            SetDefaultProbabilities();
-            SetProbabilityUpgradeFactors();
+            probabilities = new Dictionary<int, float>();
+            SetProbabilityUpgradeFactorsOnHit();
         }
 
         public IEnumerable<RDeepBet> GoForBet(RDeepPlayer player, List<int> LastNumbers)
@@ -46,17 +46,16 @@ namespace RDeepCore
             return result;
         }
 
-        private void SetProbabilityUpgradeFactors()
+        private void SetProbabilityUpgradeFactorsOnHit()
         {
-            probabilityUpgradeFactors.Add(PositonTypeCategory.Even, new List<int> { -5, 0, 0, 8, 2, 1, 1, 1, 1, 1 });
-            probabilityUpgradeFactors.Add(PositonTypeCategory.Third, new List<int> { -2, +2, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
-            probabilityUpgradeFactors.Add(PositonTypeCategory.Straight, new List<int> { 1 });
+            probabilityUpgradeFactorsOnHit = new Dictionary<PositonTypeCategory, List<int>>();
+            probabilityUpgradeFactorsOnHit.Add(PositonTypeCategory.Even, new List<int> { 10, 5, 0, 0, -8, -2, -1, -1, -1, -1, -1 });
+            probabilityUpgradeFactorsOnHit.Add(PositonTypeCategory.Third, new List<int> { 15, 2, -2, 0, 0, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 });
+            probabilityUpgradeFactorsOnHit.Add(PositonTypeCategory.Straight, new List<int> { 220, -1 });
         }
 
         private void SetDefaultProbabilities()
         {
-            probabilities = new Dictionary<int, float>();
-
             wheelNumbers = RDeepPositions.rDeepPositions.Where(num => num.isWheelNumber == true);
 
             foreach (RDeepPosition number in wheelNumbers)
@@ -67,11 +66,32 @@ namespace RDeepCore
 
         private void UpdateProbabilities(List<int> LastNumbers)
         {
+            SetDefaultProbabilities();
+
             RDeepPosition currentPos = RDeepPositions.GetPositionByID(LastNumbers[LastNumbers.Count - 1]);
             foreach(RDeepPosition number in wheelNumbers)
             {
                 //probabilities[number.ID] = 
             }
+        }
+
+        private void UpdateProbabilitiesByPosType(PositonType posType)
+        {
+
+        }
+
+        private PositonTypeCategory GetPositonTypeCategory(PositonType posType)
+        {
+            if (posType == PositonType.Black || posType == PositonType.Red || posType == PositonType.Odd || posType == PositonType.Even || posType == PositonType.Low || posType == PositonType.High)
+            {
+                return PositonTypeCategory.Even;
+            }
+            else if (posType == PositonType.FirstColumn || posType == PositonType.SecondColumn || posType == PositonType.ThirdColumn || posType == PositonType.FirstDozen || posType == PositonType.SecondDozen || posType == PositonType.ThirdDozen)
+            {
+                return PositonTypeCategory.Third;
+            }
+            else
+                return PositonTypeCategory.Straight;
         }
     }
 
